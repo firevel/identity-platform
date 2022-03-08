8fb133c1a92e086223fbdc9ea8862ee8191433ca
Michael Slowik <slowik.michael@gmail.com>
2022-03-07 21:58:43 -0500

    identity-platform:batch-create

---
+++ src/Commands/IdentityPlatformBatchCreate.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Firevel\IdentityPlatform\Commands;
+
+use Firevel\IdentityPlatform\Services\IdentityPlatformService;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class IdentityPlatformBatchCreate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'identity-platform:batch-create {--tenant=} {--chunk-size=100}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import firebase users from Laravel using Google Identity Platform API.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $tenant = $this->option('tenant');
+
+        app(config('identityplatform.users.model'))
+            ->chunk(
+                $this->option('chunk-size'),
+                function($users) use($tenant) {
+                    $users->transform(function ($user) use($tenant) {
+                        if (method_exists($user, 'toFirebaseUser')) {
+                            $userData = $this->toFirebaseUser();
+                        } else {
+                            $userData = [
+                                'localId' => $user->id,
+                                'email' => $user->email,
+                                'displayName' => $user->name,
+                                'passwordHash' => $user->password,
+                            ];
+                        }
+                        if (!empty($tenant)) {
+                            $userData['tenantId'] = $tenant;
+                        }
+                        return $userData;
+                    });
+                    IdentityPlatform::batchCreate($users);
+                }
+            );
+        return 0;
+    }
+}
